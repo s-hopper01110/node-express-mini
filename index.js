a66235c4fde1c202e3f57b51e12e0e9de2e00af6
Sheila <s.hopper01110@gmail.com>
2019-02-16 17:30:22 -0500

    Initial Commit

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // implement your API here
+//like import express from express
+const express = require('express');
+
+//pulls in db.js file
+const db = require('./data/db.js');
+
+
+const server = express();
+
+//middleware
+server.use(express.json());
+
+
+
+
+//route handler ( GET ) 
+server.get('/', (req, res) => {
+    res.send('express project -- success')
+})
+
+// //When the client makes a GET request to /api/users:
+
+// If there's an error in retrieving the users from the database:
+// cancel the request.
+// respond with HTTP status code 500.
+// return the following JSON object: { error: "The users information could not be retrieved." }.//
+
+server.get('/api/users', (req, res ) => {
+    db.find()
+    .then(users => {
+        res.status(200).json({ success: true, users })//headers
+    .catch(err => {
+        res.status(500).json({ success: false, message: 'The users information could not be retrieved.'
+    })
+}
+
+    
+
+
+
+
+// When the client makes a GET request to /api/users/:id:
+
+// If the user with the specified id is not found:
+
+// return HTTP status code 404 (Not Found).
+// return the following JSON object: { message: "The user with the specified ID does not exist." }.
+// If there's an error in retrieving the user from the database:
+
+// cancel the request.
+// respond with HTTP status code 500.
+// return the following JSON object: { error: "The user information could not be retrieved." }.
+
+
+
+
+
+server.listen(4000, () => {
+    console.log('\n*** Running on port 4000 ***\n');
+})
